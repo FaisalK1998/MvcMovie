89510da88afbc0e9b6c4a8e6f712d3e29953ffc1
Faisal Khan <faisalalam98@gmail.com>
2018-04-05 23:58:39 +0100

    Adding search by genre

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        //List of Movies
+        public List<Movie> movies;
+
+        //SelectList contains the list of genres
+        public SelectList genres;
+
+        //Contains the selected genre
+        public string movieGenre { get; set; }
+    }
+}
