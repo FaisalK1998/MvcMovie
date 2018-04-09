948ec038f793aaa816b41baff1ba59411407fa81
Faisal Khan <faisalalam98@gmail.com>
2018-04-09 22:18:25 +0100

    Adding a Rating Property to the Movie Model

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,26 +22,29 @@ namespace MvcMovie.Models
                 context.Movie.AddRange(
                    new Movie
                    {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-1-11"),
-                        Genre = "Romantic Comedy",
-                        Price = 7.99M
+                       Title = "When Harry Met Sally",
+                       ReleaseDate = DateTime.Parse("1989-1-11"),
+                       Genre = "Romantic Comedy",
+                       Rating = "R",
+                       Price = 7.99M
                    },
 
                    new Movie
                    {
-                        Title = "Ghostbusters ",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "Comedy",
-                        Price = 8.99M
+                       Title = "Ghostbusters ",
+                       ReleaseDate = DateTime.Parse("1984-3-13"),
+                       Genre = "Comedy",
+                       Rating = "R",
+                       Price = 8.99M
                    },
 
                    new Movie
                    {
-                         Title = "Ghostbusters 2",
-                         ReleaseDate = DateTime.Parse("1986-2-23"),
-                         Genre = "Comedy",
-                         Price = 9.99M
+                       Title = "Ghostbusters 2",
+                       ReleaseDate = DateTime.Parse("1986-2-23"),
+                       Genre = "Comedy",
+                       Rating = "R",
+                       Price = 9.99M
                    },
 
                    new Movie
@@ -49,6 +52,7 @@ namespace MvcMovie.Models
                        Title = "Rio Bravo",
                        ReleaseDate = DateTime.Parse("1959-4-15"),
                        Genre = "Western",
+                       Rating = "R",
                        Price = 3.99M
                    }
                 );
