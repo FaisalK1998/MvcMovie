bbe7e80594e6c9baae3d42d370964a533d942458
Faisal Khan <faisalalam98@gmail.com>
2018-04-11 18:54:24 +0100

    Adding Validation

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,6 +6,9 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         //Display attribute specifies what to display for the name of the field
@@ -14,8 +17,18 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        public string Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
