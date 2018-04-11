6683fac4763561a7d0cf69d1cff473ba9721f2f4
Faisal Khan <faisalalam98@gmail.com>
2018-04-11 19:06:01 +0100

    Combining attributes on one line

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -15,20 +15,18 @@ namespace MvcMovie.Models
         //DataType attribute specifies the type of data (Date)
         [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
+        //Combining attributes on one line
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; }
     }
 }
